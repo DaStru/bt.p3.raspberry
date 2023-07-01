6be8ba618bc6e29779e7d22b240914558289a147
DaStru <@>
2023-07-01 16:59:10 +0200

    Create list of commands

--- test_mopidy.py
+++ test_mopidy.py
@@ -1,12 +1,14 @@
 import requests
 import json
+import sys
 
 mopidy_url = "http://localhost:6680/mopidy/rpc"
 
 payload = {
         "jsonrpc": "2.0",
         "id": 1,
-        "method": "core.describe"
+        "method": sys.argv[1],
+        "params": sys.argv[2] if sys.argv[1] else []
 }
 
 response = requests.post(mopidy_url, json=payload).json()
